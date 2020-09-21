6a6ac5f8b63ebf0ee5f03f43610bd0a2fd685371
Chris <ckeenan@starry.com>
2020-09-21 11:02:48 -0400

    Created buildSrc module with LibraryConstants object

---
+++ buildSrc/src/main/java/com.chrynan.uri.buildSrc/LibraryConstants.kt
@@ -0,0 +1,17 @@
+package com.chrynan.uri.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.uri"
+    const val owner = "chrynan"
+    const val repoName = "uri"
+    const val versionName = "0.1.0"
+    const val versionCode = 1
+    const val versionDescription = "Pre-release 0.1.0"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/uri.git"
+    const val androidCompileSdkVersion = 29
+    const val androidMinSdkVersion = 23
+    const val androidTargetSdkVersion = 29
+}
