024936e5f993d2a50c6a661887fb5994d5eac166
Chris <ckeenan@starry.com>
2020-09-21 15:10:46 -0400

    Added KDoc comment to InvalidUriException class

--- uri-core/src/commonMain/kotlin/com.chrynan.uri.core/InvalidUriException.kt
+++ uri-core/src/commonMain/kotlin/com.chrynan.uri.core/InvalidUriException.kt
@@ -1,4 +1,7 @@
 package com.chrynan.uri.core
 
-data class InvalidUriException(override val message: String? = null) :
+/**
+ * An Exception that is thrown when attempting to obtain a [Uri] with invalid or not properly formatted parts.
+ */
+class InvalidUriException(override val message: String? = null) :
     RuntimeException("Invalid Uri: message = $message")
