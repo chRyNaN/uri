1b93b1e9c9f13c86045b275f78bdaa36c7768f54
Chris <ckeenan@starry.com>
2020-09-21 15:03:00 -0400

    Removed redundant overrides of authority and schemeSpecificPart properties in SimpleUriWithParts class

--- uri-core/src/commonMain/kotlin/com.chrynan.uri.core/SimpleUriWithParts.kt
+++ uri-core/src/commonMain/kotlin/com.chrynan.uri.core/SimpleUriWithParts.kt
@@ -10,43 +10,6 @@ internal data class SimpleUriWithParts(
     override val fragment: String? = null
 ) : Uri {
 
-    override val authority: String?
-        get() {
-            if (userInfo == null && host == null && port == null) return null
-
-            return buildString {
-                if (userInfo != null) {
-                    append(userInfo)
-                    append('@')
-                }
-
-                if (host != null) {
-                    append(host)
-                }
-
-                if (port != null) {
-                    append(':')
-                    append(port)
-                }
-            }
-        }
-
-    override val schemeSpecificPart: String
-        get() = buildString {
-            if (authority != null) {
-                append("//")
-                append(authority)
-            }
-
-            append('/')
-            append(path)
-
-            if (query != null) {
-                append('?')
-                append(query)
-            }
-        }
-
     override val uriString: UriString
-        get() = "$scheme$schemeSpecificPart"
+        get() = "$scheme:$schemeSpecificPart"
 }
