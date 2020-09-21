42ac5bc94fb8e70b24f6bbef0cba5bdc7907d7ed
Chris <ckeenan@starry.com>
2020-09-21 15:05:19 -0400

    Added KDoc to SimpleUriWithParts class

--- uri-core/src/commonMain/kotlin/com.chrynan.uri.core/SimpleUriWithParts.kt
+++ uri-core/src/commonMain/kotlin/com.chrynan.uri.core/SimpleUriWithParts.kt
@@ -1,5 +1,10 @@
 package com.chrynan.uri.core
 
+/**
+ * An implementation of the [Uri] interface using the provided parts. Note that the provided parts are already expected
+ * to be validated. If the provided parts are not validated and formatted correctly, then the behavior of properties
+ * and functions is undefined.
+ */
 internal data class SimpleUriWithParts(
     override val scheme: String,
     override val userInfo: String? = null,
