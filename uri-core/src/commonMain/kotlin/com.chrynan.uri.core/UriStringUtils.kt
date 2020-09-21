b38db7c6d1dc41c7166338f3285d79793348a562
Chris <ckeenan@starry.com>
2020-09-21 15:13:30 -0400

    Updated UriStringUtils with KDoc comments

--- uri-core/src/commonMain/kotlin/com.chrynan.uri.core/UriStringUtils.kt
+++ uri-core/src/commonMain/kotlin/com.chrynan.uri.core/UriStringUtils.kt
@@ -7,12 +7,28 @@ import com.chrynan.validator.ValidationResult
 
 private val validator = UrlValidator()
 
+/**
+ * Retrieves a [Uri] from the provided [uriString]. If the provided [uriString] is valid and properly formatted, then a
+ * [Uri] instance will be returned. If the provided [uriString] is invalid or is not properly formatted, then the
+ * [InvalidUriException] will be thrown.
+ *
+ * @see [Uri]
+ * @see [UriString]
+ */
 fun Uri.Companion.fromString(uriString: UriString): Uri =
     when (val result = validator.validate(uriString)) {
         is ValidationResult.Valid -> SimpleUriWithString(uriString)
         is ValidationResult.Invalid -> throw InvalidUriException(message = "Invalid Uri from String = $uriString. Errors = ${result.errors}")
     }
 
+/**
+ * Retrieves a [Uri] from the provided [uriString]. If the provided [uriString] is valid and properly formatted, then a
+ * [Uri] instance will be returned. If the provided [uriString] is invalid or is not properly formatted, then the null
+ * will be returned.
+ *
+ * @see [Uri]
+ * @see [UriString]
+ */
 fun Uri.Companion.fromStringOrNull(uriString: UriString): Uri? =
     try {
         fromString(uriString = uriString)
