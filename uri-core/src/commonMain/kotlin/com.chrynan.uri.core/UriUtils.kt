1efe14864113d3e673fc981a06c2a441a9aea03a
Chris <ckeenan@starry.com>
2020-09-21 18:01:59 -0400

    Updated Uri.queryParameters() function in UriUtils

--- uri-core/src/commonMain/kotlin/com.chrynan.uri.core/UriUtils.kt
+++ uri-core/src/commonMain/kotlin/com.chrynan.uri.core/UriUtils.kt
@@ -84,21 +84,35 @@ val Uri.slug: String?
 
 /**
  * Retrieves a [List] of [String]s each representing the individual Path Segments of this [Uri]. A Path Segment is a
- * portion of the [Uri.path] which is separated by slash characters ('/').
+ * portion of the [Uri.path] component which is separated by slash characters ('/'). If there are no Path Segments,
+ * then this will return an empty [List].
  */
 val Uri.pathSegments: List<String>
     get() = path.split('/')
 
-val Uri.queryParameters: Map<String, String>
-    get() {
-        val q = query ?: return emptyMap()
+/**
+ * Retrieves a [Map] of [String] keys and values representing the individual Query Parameters in this [Uri]. A Query
+ * Parameter is a portion of the [Uri.query] component that is separated by other Query Parameters by the provided
+ * [delimiters]. A Query Parameter may be in the key-value form with an equals character ('=') separating the key and
+ * value. The resulting [Map] will have a key matching the Query Parameters keys and corresponding values. If a Query
+ * Parameter doesn't have a value (the equals sign isn't present), then the value will be considered null. If there are
+ * no Query Parameters, then this will return an empty [Map].
+ *
+ * @param [delimiters] The delimiters that separate the Query Parameters in the [Uri.query] component.
+ * @param [ignoreCase] Whether to ignore the case (upper/lower) of the delimiters or not.
+ */
+fun Uri.queryParameters(
+    vararg delimiters: Char = charArrayOf('&', ';'),
+    ignoreCase: Boolean = true
+): Map<String, String?> {
+    val q = query ?: return emptyMap()
 
-        return q.split('&', ';', ignoreCase = true, limit = q.length)
-            .associate {
-                val equalsIndex = it.indexOf(char = '=')
-                val key = if (equalsIndex == -1) it else it.substring(startIndex = 0, endIndex = equalsIndex)
-                val value = if (equalsIndex == -1) "" else it.substring(startIndex = equalsIndex)
+    return q.split(*delimiters, ignoreCase = ignoreCase)
+        .associate {
+            val equalsIndex = it.indexOf(char = '=')
+            val key = if (equalsIndex == -1) it else it.substring(startIndex = 0, endIndex = equalsIndex)
+            val value = if (equalsIndex == -1) null else it.substring(startIndex = equalsIndex)
 
-                key to value
-            }
-    }
+            key to value
+        }
+}
