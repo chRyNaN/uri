7bff2162b91fe91cf5d68471146ffd7c3b80443d
Chris <ckeenan@starry.com>
2020-09-21 15:37:58 -0400

    Added KDoc comments to the UriString typealias

--- uri-core/src/commonMain/kotlin/com.chrynan.uri.core/UriString.kt
+++ uri-core/src/commonMain/kotlin/com.chrynan.uri.core/UriString.kt
@@ -1,3 +1,12 @@
 package com.chrynan.uri.core
 
+/**
+ * A [UriString] is a [String] representation of a complete [Uri].
+ *
+ * Note that no verification is performed to assert that a [UriString] is valid and properly formatted. To verify that
+ * a [UriString] is valid, convert it to a [Uri] using the [Uri.Companion.fromString] or
+ * [Uri.Companion.fromStringOrNull] functions.
+ *
+ * Note that [UriString] is just a typealias to [String].
+ */
 typealias UriString = String
