750efadc6ede665023b9d43613a48b009d8069a5
Christopher Keenan <ckeenan@starry.com>
2022-02-12 16:35:59 -0600

    Bumped version to 0.3.2 (5)

--- buildSrc/src/main/java/com.chrynan.uri.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.uri.buildSrc/LibraryConstants.kt
@@ -2,13 +2,12 @@ package com.chrynan.uri.buildSrc
 
 object LibraryConstants {
 
-    const val bintrayRepo = "chrynan"
     const val group = "com.chrynan.uri"
     const val owner = "chrynan"
     const val repoName = "uri"
-    const val versionName = "0.3.1"
-    const val versionCode = 4
-    const val versionDescription = "Release 0.3.1"
+    const val versionName = "0.3.2"
+    const val versionCode = 5
+    const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/uri.git"
 
